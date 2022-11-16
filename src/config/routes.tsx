a79517eb603e4836c2e0be4e856b0a51359c572a
xishuianfeng <xishuianfeng@gmail.com>
2022-11-16 15:05:01 +0800

    重构main.ts

---
+++ src/config/routes.tsx
@@ -0,0 +1,7 @@
+import { Foo } from '../views/Foo'
+import { Bar } from '../views/Bar'
+
+export const routes=[
+    {path:'/',component:Foo},
+    {path:'/Bar',component:Bar},
+]
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import { createApp } from 'vue'
 import { App } from './App'
 import './style.css'
-import {createRouter,createWebHashHistory} from 'vue-router'
-import { Foo } from './views/Foo'
-import { Bar } from './views/Bar'
+import {createRouter } from 'vue-router'
+import { routes } from './config/routes'
+import { history } from './shared/history'
 
-const routes=[
-    {path:'/',component:Foo},
-    {path:'/Bar',component:Bar},
-]
+const router = createRouter({history,routes,})
 
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes, // `routes: routes` 的缩写
-  })
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
