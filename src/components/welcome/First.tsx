c1e33403f5c8d7e8fcad0f2177a48e02d43a7d8a
xishuianfeng <xishuianfeng@gmail.com>
2022-11-17 13:55:39 +0800

    4个欢迎页面完成

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -8,13 +8,13 @@ export const First = defineComponent({
     return ()=> (
       <div class={s.wrapper}>
         <div class={s.card}>
-          <img class={s.pig} src={pig}/>
+          <img src={pig}/>
           <h2>会挣钱，<br/>还要会省钱</h2>
         </div>
         <div class={s.actions}>
           <RouterLink class={s.fake} to={"/welcome/start"}>跳过</RouterLink>
           <RouterLink to={"/welcome/2"}>下一页</RouterLink>
-          <RouterLink to={"/welcome/start"}>跳过</RouterLink>
+          <RouterLink to={"/start"}>跳过</RouterLink>
         </div>
       </div>
     )
--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,9 +1,21 @@
 import { defineComponent } from 'vue';
+import s from './First.module.scss';
+import cloud from '../../assets/icons/cloud.svg'
+import { RouterLink } from 'vue-router';
+
 export const Forth = defineComponent({
   setup:(props,context) => {
     return ()=> (
-      <div>
-
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img src={cloud}/>
+          <h2>云备份，<br/>不怕数据丢失</h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to={"/welcome/start"}>跳过</RouterLink>
+          <RouterLink to={"/start"}>完成</RouterLink>          
+          <RouterLink class={s.fake} to={"/welcome/start"}>跳过</RouterLink>
+        </div>
       </div>
     )
   }
