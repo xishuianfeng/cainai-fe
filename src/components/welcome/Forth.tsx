9d6bd592c7462d152de2eaf760b87cb8b01496d8
xishuianfeng <xishuianfeng@gmail.com>
2022-11-17 14:28:01 +0800

    重构 提取WelcomeLayout 组件

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,22 +1,22 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './WelcomeLayout.module.scss';
 import cloud from '../../assets/icons/cloud.svg'
 import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
 
 export const Forth = defineComponent({
   setup:(props,context) => {
+    const slots = {
+      icon:()=> <img src={cloud}/>,
+      title:()=> <h2>云备份，<br/>不怕数据丢失</h2>,
+      buttons:()=><>
+          <RouterLink class={s.fake} to={"/welcome/start"}>跳过</RouterLink>
+          <RouterLink to={"/start"}>完成</RouterLink>
+          <RouterLink to={"/start"}>跳过</RouterLink>
+          </>,
+    }
     return ()=> (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img src={cloud}/>
-          <h2>云备份，<br/>不怕数据丢失</h2>
-        </div>
-        <div class={s.actions}>
-          <RouterLink class={s.fake} to={"/welcome/start"}>跳过</RouterLink>
-          <RouterLink to={"/start"}>完成</RouterLink>          
-          <RouterLink class={s.fake} to={"/welcome/start"}>跳过</RouterLink>
-        </div>
-      </div>
+      <WelcomeLayout v-slots={slots}></WelcomeLayout>
     )
   }
 })
--- src/components/welcome/Second.tsx
+++ src/components/welcome/Second.tsx
@@ -1,22 +1,22 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './WelcomeLayout.module.scss';
 import clock from '../../assets/icons/clock.svg'
 import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
 
 export const Second = defineComponent({
   setup:(props,context) => {
-    return ()=> (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img src={clock}/>
-          <h2>每日提醒，<br/>不遗漏每一笔账单</h2>
-        </div>
-        <div class={s.actions}>
+    const slots = {
+      icon:()=> <img src={clock}/>,
+      title:()=> <h2>每日提醒，<br/>不遗漏每一笔账单</h2>,
+      buttons:()=><>
           <RouterLink class={s.fake} to={"/welcome/start"}>跳过</RouterLink>
           <RouterLink to={"/welcome/3"}>下一页</RouterLink>
           <RouterLink to={"/start"}>跳过</RouterLink>
-        </div>
-      </div>
+          </>,
+    }
+    return ()=> (
+      <WelcomeLayout v-slots={slots}></WelcomeLayout>
     )
   }
 })
