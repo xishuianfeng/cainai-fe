72cf4355b8ff2064e509597e81ebdd60ce1bd78c
“xishuianfeng” <xishuianfeng@gmail.com>
2022-11-20 20:19:58 +0800

    完成浮动窗口

---
+++ src/shared/Overlay.tsx
@@ -0,0 +1,54 @@
+import { defineComponent, PropType} from 'vue';
+import Icon from '../shared/Icon.vue'
+import s from './Overlay.module.scss'
+import { RouterLink } from 'vue-router';
+
+export const Ovaerlay = defineComponent({
+  props:{
+    onClose: {
+      type:Function,
+    }
+  },
+  setup:(props,context) => {
+    const onClickSignIn = () => { }
+
+    const close =() => {
+      props.onClose?.()
+    }
+
+    return ()=> (
+      <>
+      <div class={s.mask} onClick={close}>
+        <div class={s.overlay}>
+           <section class={s.currentUser} onClick={onClickSignIn}>
+            <h2>用户</h2>
+            <p>点击登录</p>
+          </section>
+          <nav>
+            <ul class={s.action_list}>
+              <li class={s.flex}>
+                <RouterLink to="/statistics" class={s.action}>
+                  <Icon icon="chart" class={s.icon} />
+                  <span>统计图表</span>
+                </RouterLink>
+              </li>
+              <li class={s.flex}>
+                <RouterLink to="/export" class={s.action}>
+                  <Icon icon="export" class={s.icon}/>
+                  <span>导出数据</span>
+                </RouterLink>
+              </li>
+              <li class={s.flex}>
+                <RouterLink to="/notify" class={s.action}>
+                  <Icon icon="notify" class={s.icon}/>
+                  <span>记账提醒</span>
+                </RouterLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
+    )
+  }
+})
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,17 +1,22 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { Button } from '../shared/Button';
 import { FloatButton } from '../shared/FloatButton';
 import Icon from '../shared/Icon.vue'
 import { Navbar } from '../shared/Narbar';
+import { Ovaerlay } from '../shared/Overlay';
 import s from './StartPage.module.scss'
 export const StartPage = defineComponent({
   setup:(props,context) => {
     const onClick=()=>{
       console.log(1);
-      
+    }
+
+    const refOverlayVisible = ref(false)
+    const onClickMenu = () => {
+      refOverlayVisible.value = !refOverlayVisible.value      
     }
     const slots = {
-      icon:()=> <Icon icon='menu' class={s.navIcon}/>,
+      icon:()=> <Icon icon='menu' class={s.navIcon} onClick={onClickMenu}/>,
       default:() => <span>彩乃记账</span>
     }
     return ()=> (
@@ -24,6 +29,7 @@ export const StartPage = defineComponent({
           <Button class={s.button} onClick={onClick}>开始记账</Button>
         </div>
         <FloatButton iconName='add'/>
+        {refOverlayVisible.value && <Ovaerlay onClose={()=>refOverlayVisible.value = false}/>}
       </div>
     )
   }
