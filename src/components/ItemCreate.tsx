9cb10f292e87fe4c691ab6f6c4deb7b86fe48206
“xishuianfeng” <xishuianfeng@gmail.com>
2022-11-21 16:57:17 +0800

    s.class 添加无效

--- src/components/ItemCreate.tsx
+++ src/components/ItemCreate.tsx
@@ -1,16 +1,26 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { MainLayout } from '../layout/MainLayout';
 import s from './ItemCreate.module.scss'
 import Icon from '../shared/Icon.vue'
+import { Tabs,Tab } from '../shared/Tabs';
 export const ItemCreate = defineComponent({
   setup:(props,context) => {
+    const refKind = ref('支出')
+    const onUpdateSelected = (name:string) =>refKind.value = name
     return ()=> (
       <MainLayout>
         {{
           title:()=>'记一笔',
           icon:<Icon icon='left' class={s.navIcon} />,
           default:()=><>
-          <div>main</div>
+            <Tabs selected={refKind.value} onUpdateSelected={onUpdateSelected}>
+              <Tab name='支出'>
+                icon 列表
+              </Tab>
+              <Tab name='收入'>
+                icon 列表2
+              </Tab>
+            </Tabs>
           </>
         }}
       </MainLayout>
---
+++ src/shared/Tabs.tsx
@@ -0,0 +1,41 @@
+import { defineComponent } from 'vue';
+import s from './Tabs.module.scss'
+export const Tabs = defineComponent({
+  props:{
+    selected:{
+      type:String,
+    }
+  },
+  setup:(props,context) => {
+    return ()=> {
+      const array = context.slots.default?.()
+      if(!array) return () => null
+      for(let i = 0;i<array.length;i++){
+        if(array[i].type !== Tab){
+          throw new Error('<Tabs> only accepts <Tab> as children')
+        }
+      }
+      return( 
+          <div class={s.tabs}>
+            <ol class={s.tabs_nav}>
+              {array.map(item=><li class={item.props?.name === props.selected ? 's.selected':''}>{item.props?.name}</li>)}
+            </ol>
+          <div></div>
+        </div>
+      )
+    }
+  }
+})
+
+export const Tab = defineComponent({
+  props:{
+    name:{
+      type:String,
+    }
+  },
+  setup:(props,context) => {
+    return ()=> (
+      <div>{context.slots.default?.()}</div>
+    )
+  }
+})
