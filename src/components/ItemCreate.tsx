b4eb0543571b958d506960d4f94238feff328754
“xishuianfeng” <xishuianfeng@gmail.com>
2022-11-21 14:09:23 +0800

    正在实现/items/create页面

--- src/components/ItemCreate.tsx
+++ src/components/ItemCreate.tsx
@@ -1,9 +1,19 @@
 import { defineComponent } from 'vue';
+import { MainLayout } from '../layout/MainLayout';
 import s from './ItemCreate.module.scss'
+import Icon from '../shared/Icon.vue'
 export const ItemCreate = defineComponent({
   setup:(props,context) => {
     return ()=> (
-      <div></div>
+      <MainLayout>
+        {{
+          title:()=>'记一笔',
+          icon:<Icon icon='left' class={s.navIcon} />,
+          default:()=><>
+          <div>main</div>
+          </>
+        }}
+      </MainLayout>
     )
   }
 })
--- src/views/ItemPage.tsx
+++ src/views/ItemPage.tsx
@@ -1,9 +1,13 @@
 import { defineComponent } from 'vue';
+import { MainLayout } from '../layout/MainLayout';
 import s from './ItemPage.module.scss'
+import Icon from '../shared/Icon.vue'
+import { RouterView } from 'vue-router';
+
 export const ItemPage = defineComponent({
   setup:(props,context) => {
     return ()=> (
-      <div></div>
+      <RouterView />
     )
   }
 })
