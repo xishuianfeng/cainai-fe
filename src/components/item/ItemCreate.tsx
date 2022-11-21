73750fbefb71c8125d2675860b9365687ddaa147
“xishuianfeng” <xishuianfeng@gmail.com>
2022-11-21 19:41:18 +0800

    制作数字按键

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,8 +1,10 @@
 import { defineComponent, ref } from 'vue';
-import { MainLayout } from '../layout/MainLayout';
+import { MainLayout } from '../../layout/MainLayout';
 import s from './ItemCreate.module.scss'
-import Icon from '../shared/Icon.vue'
-import { Tabs,Tab } from '../shared/Tabs';
+import Icon from '../../shared/Icon.vue'
+import { Tabs,Tab } from '../../shared/Tabs';
+import { InputPad } from './InputPad';
+
 export const ItemCreate = defineComponent({
   setup:(props,context) => {
     const refKind = ref('支出')
@@ -20,6 +22,9 @@ export const ItemCreate = defineComponent({
                 icon 列表2
               </Tab>
             </Tabs>
+            <div class={s.inputPad_wrapper}>
+              <InputPad />
+            </div>
           </>
         }}
       </MainLayout>
--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -5,8 +5,8 @@ import { Third } from '../components/welcome/Third'
 import { Forth } from '../components/welcome/Forth'
 import { StartPage } from '../views/StartPage'
 import { ItemPage } from '../views/ItemPage'
-import { ItemList } from '../components/ItemList'
-import { ItemCreate } from '../components/ItemCreate'
+import { ItemList } from '../components/item/ItemList'
+import { ItemCreate } from '../components/item/ItemCreate'
 
 export const routes=[
   {path:'/',component:Welcome},
