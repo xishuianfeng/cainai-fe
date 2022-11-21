f23c0764f5bc950a537ec42329798b16305b15ec
“xishuianfeng” <xishuianfeng@gmail.com>
2022-11-21 13:56:28 +0800

    创建MainLayout布局

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -2,10 +2,11 @@ import { defineComponent, ref } from 'vue';
 import { Button } from '../shared/Button';
 import { FloatButton } from '../shared/FloatButton';
 import Icon from '../shared/Icon.vue'
-import { Navbar } from '../shared/Narbar';
+import { Navbar } from '../shared/Navbar';
 import { Ovaerlay } from '../shared/Overlay';
 import { RouterLink } from 'vue-router';
 import s from './StartPage.module.scss'
+import { MainLayout } from '../layout/MainLayout';
 
 
 export const StartPage = defineComponent({
@@ -14,13 +15,31 @@ export const StartPage = defineComponent({
     const onClickMenu = () => {
       refOverlayVisible.value = !refOverlayVisible.value      
     }
-    const slots = {
-      icon:()=> <Icon icon='menu' class={s.navIcon} onClick={onClickMenu}/>,
-      default:() => <span>彩乃记账</span>
-    }
     return ()=> (
-      <div>
-        <Navbar v-slots={slots}></Navbar>
+      // <div>
+        <MainLayout>{
+          {
+            title:()=>'彩乃记账',
+            icon:()=><Icon icon='menu' class={s.navIcon} onClick={onClickMenu}/>,
+            default:()=><>
+          <div class={s.pig_wrapper}>
+            <Icon icon='pig' class={s.pig}/>
+          </div>
+          <div class={s.button_wrapper}>
+            <RouterLink to="/items/create">
+              <Button class={s.button} >开始记账</Button>
+            </RouterLink>
+          </div>
+          <RouterLink to="/items/create">
+            <FloatButton iconName='add' class={s.floatbutton}/>
+          </RouterLink>
+          {refOverlayVisible.value && <Ovaerlay onClose={()=>refOverlayVisible.value = false}/>}
+        </>
+      
+        }  
+        }</MainLayout>
+
+        /* <Navbar v-slots={slots}></Navbar>
         <div class={s.pig_wrapper}>
           <Icon icon='pig' class={s.pig}/>
         </div>
@@ -33,7 +52,7 @@ export const StartPage = defineComponent({
           <FloatButton iconName='add' class={s.floatbutton}/>
         </RouterLink>
         {refOverlayVisible.value && <Ovaerlay onClose={()=>refOverlayVisible.value = false}/>}
-      </div>
+      </div> */
     )
   }
 })
