a15930d744918be3cf06ec68069578dd33636a3b
“xishuianfeng” <xishuianfeng@gmail.com>
2022-11-22 17:34:19 +0800

    完成 TagCreate 样式

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -7,9 +7,12 @@ import { StartPage } from '../views/StartPage'
 import { ItemPage } from '../views/ItemPage'
 import { ItemList } from '../components/item/ItemList'
 import { ItemCreate } from '../components/item/ItemCreate'
+import { TagCreate } from '../components/tag/TagCreate'
+import { TagEdit } from '../components/tag/TagEdit'
+import { TagPage } from '../views/TagsPage'
 
 export const routes=[
-  {path:'/',component:Welcome},
+  {path:'/',redirect:'/welcome/1'},
   {
     path:'/welcome',
     component:Welcome,
@@ -27,6 +30,13 @@ export const routes=[
     children:[
       {path:'',component:ItemList},
       {path:'create',component:ItemCreate},
+    ]
+  },
+  {
+    path:'/tags',component:TagPage,
+    children:[
+      {path:'create',component:TagCreate},
+      {path:':id',component:TagEdit}
     ]
   }
 ]
---
+++ src/views/TagsPage.tsx
@@ -0,0 +1,9 @@
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+export const TagPage = defineComponent({
+  setup:(props,context) => {
+    return ()=> (
+      <RouterView />
+    )
+  }
+})
