a2746ae5e36a64fa7524aea65d425a58d88f4f8b
“xishuianfeng” <xishuianfeng@gmail.com>
2022-11-22 13:38:48 +0800

    重写 inputPad 布局

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,36 +1,46 @@
 import { time } from '../../shared/time';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import Icon from '../../shared/Icon.vue'
 import s from './InputPad.module.scss'
+import { DatetimePicker,Popup } from 'vant';
+
 export const InputPad = defineComponent({
   setup:(props,context) => {
+    const refDate = ref<Date>()
+    const appendText = (n:number|string) => refAmount.value += n.toString()
     const buttons=[
-      {text:'1',onClick:()=>{}},
-      {text:'2',onClick:()=>{}},
-      {text:'3',onClick:()=>{}},
-      {text:'清空',onClick:()=>{}},
-      {text:'4',onClick:()=>{}},
-      {text:'5',onClick:()=>{}},
-      {text:'6',onClick:()=>{}},
-      {text:'+',onClick:()=>{}},
-      {text:'7',onClick:()=>{}},
-      {text:'8',onClick:()=>{}},
-      {text:'9',onClick:()=>{}},
-      {text:'-',onClick:()=>{}},
-      {text:'.',onClick:()=>{}},
-      {text:'0',onClick:()=>{}},
-      {text:'删',onClick:()=>{}},
+      {text:'1',onClick:()=>{appendText(1)}},
+      {text:'2',onClick:()=>{appendText(2)}},
+      {text:'3',onClick:()=>{appendText(3)}},
+      {text:'4',onClick:()=>{appendText(4)}},
+      {text:'5',onClick:()=>{appendText(5)}},
+      {text:'6',onClick:()=>{appendText(6)}},
+      {text:'7',onClick:()=>{appendText(7)}},
+      {text:'8',onClick:()=>{appendText(8)}},
+      {text:'9',onClick:()=>{appendText(9)}},
+      {text:'.',onClick:()=>{appendText('.')}},
+      {text:'0',onClick:()=>{appendText(0)}},
+      {text:'清空',onClick:()=>{refAmount.value = ''}},
       {text:'提交',onClick:()=>{}},
     ]
+    const refShowPop = ref(false)
+    const setDate = (date:Date)=>{refDate.value = date;refShowPop.value = false}
+    const refAmount = ref('')
     return ()=> <>
       <div class={s.dateAndAmount}>
         <span class={s.date}>
           <Icon icon='date' class={s.icon}/>
           <span>
-            2022
+            <span onClick={()=>refShowPop.value = true}>{time(refDate.value).format()}</span>
+            <Popup position='bottom' v-model:show = {refShowPop.value}>
+              <DatetimePicker value={refDate.value} type="date" title="选择年月日"
+                onConfirm={setDate}
+                onCancel={setDate}
+                />
+            </Popup>
           </span>
         </span>
-        <span class={s.amount}>648</span>
+        <span class={s.amount}>{refAmount.value}</span>
       </div>
       <div class={s.buttons}>
         {buttons.map(button => 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import styleImport,{ VantResolve } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,16 @@ export default defineConfig({
     vueJsx({
       transformOn:true,
       mergeProps:true,
-    })
+    }),
+    styleImport({
+      resolves:[VantResolve()],
+      libs:[
+        {
+          libraryName:'vant',
+          esModule:true,
+          resolveStyle:name => `../es/${name}/style`
+        }
+      ]
+    }),
   ]
 })
