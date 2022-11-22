e9151369543f6bb47e4721dc77cd06237a401f63
“xishuianfeng” <xishuianfeng@gmail.com>
2022-11-22 14:00:42 +0800

    完成数字输入功能

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -7,7 +7,33 @@ import { DatetimePicker,Popup } from 'vant';
 export const InputPad = defineComponent({
   setup:(props,context) => {
     const refDate = ref<Date>()
-    const appendText = (n:number|string) => refAmount.value += n.toString()
+
+    const appendText = (n:number|string) => {
+      const nString = n.toString()
+      const dotIndex =refAmount.value.indexOf('.')
+      if(refAmount.value.length >= 16){
+        return
+      }
+      if(dotIndex>=0 && refAmount.value.length - dotIndex>2){
+        return 
+      }
+      if(nString === '.'){
+        if( dotIndex >= 0){  //已经有小数点了
+          return
+        }
+      }else if(nString === '0'){
+        if(dotIndex === -1){  //已经没有小数点了
+          if(refAmount.value === '0' ) { //没小数点但是有 0
+            return
+          }
+        }
+      }else{
+        if(refAmount.value === '0'){
+          refAmount.value = ''
+        }
+      }
+      refAmount.value += nString
+    }
     const buttons=[
       {text:'1',onClick:()=>{appendText(1)}},
       {text:'2',onClick:()=>{appendText(2)}},
@@ -20,12 +46,12 @@ export const InputPad = defineComponent({
       {text:'9',onClick:()=>{appendText(9)}},
       {text:'.',onClick:()=>{appendText('.')}},
       {text:'0',onClick:()=>{appendText(0)}},
-      {text:'清空',onClick:()=>{refAmount.value = ''}},
+      {text:'清空',onClick:()=>{refAmount.value = '0'}},
       {text:'提交',onClick:()=>{}},
     ]
     const refShowPop = ref(false)
     const setDate = (date:Date)=>{refDate.value = date;refShowPop.value = false}
-    const refAmount = ref('')
+    const refAmount = ref('0')
     return ()=> <>
       <div class={s.dateAndAmount}>
         <span class={s.date}>
