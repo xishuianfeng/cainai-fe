729ac5aa8b386baf32c39a9c0fb0f856bdf2996d
“xishuianfeng” <xishuianfeng@gmail.com>
2022-11-23 13:19:15 +0800

    整理好emoji

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,21 +1,27 @@
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
 import { MainLayout } from '../../layout/MainLayout';
 import { Button } from '../../shared/Button';
+import { EmojiSelect } from '../../shared/EmojiSelect';
 import Icon from '../../shared/Icon.vue'
 import s from './TagCreate.module.scss'
 export const TagCreate = defineComponent({
   setup:(props,context) => {
+    const formDate = reactive({
+      name:'',
+      sign:'',
+    })
     return ()=> (
       <MainLayout>{{
         title:()=>'新建标签',
         icon:()=><Icon icon='left' onClick={()=>{}}/>,
         default: () => (
           <form class={s.form}>
+
             <div class={s.formRow}>
               <label class={s.formLabel}>
                 <span class={s.formItem_name}>标签名</span>
                 <div class={s.formItem_value}>
-                  <input class={[s.formItem, s.input, s.error]}></input>
+                  <input v-model = {formDate.name} class={[s.formItem, s.input, s.error]}></input>
                 </div>
                 <div class={s.formItem_errorHint}>
                   <span>必填</span>
@@ -26,158 +32,7 @@ export const TagCreate = defineComponent({
               <label class={s.formLabel}>
                 <span class={s.formItem_name}>符号</span>
                 <div class={s.formItem_value}>
-                  <div class={[s.formItem, s.emojiList, s.error]}>
-                    <nav>
-                      <span class={s.selected}>表情</span>
-                      <span>手势</span>
-                      <span>职业</span>
-                      <span>衣服</span>
-                      <span>动物</span>
-                      <span>自然</span>
-                      <span>食物</span>
-                      <span>运动</span>
-                      <span>表情</span>
-                      <span>手势</span>
-                      <span>职业</span>
-                      <span>衣服</span>
-                      <span>动物</span>
-                      <span>自然</span>
-                      <span>食物</span>
-                      <span>运动</span>
-                    </nav>
-                    <ol>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                    </ol>
-                  </div>
+                  <EmojiSelect class={[s.fromItem,s.emojiList,s.error]}/>
                 </div>
                 <div class={s.formItem_errorHint}>
                   <span>必填</span>
---
+++ src/shared/EmojiSelect.tsx
@@ -0,0 +1,45 @@
+import { defineComponent, ref } from 'vue';
+import s from './EmojiSelect.module.scss'
+import {emojiList} from './emojiList'
+export const EmojiSelect = defineComponent({
+  setup:(props,context) => {
+
+    const refSelected = ref(1)
+
+    const table: [string, string[]][] = [
+      ['表情', ['face-smiling', 'face-affection', 'face-tongue', 'face-hand',
+        'face-neutral-skeptical', 'face-sleepy', 'face-unwell', 'face-hat',
+        'face-glasses', 'face-concerned', 'face-negative', 'face-costume'
+      ]],
+      ['手势', ['hand-fingers-open', 'hand-fingers-partial', 'hand-single-finger',
+        'hand-fingers-closed', 'hands', 'hand-prop', 'body-parts']],
+      ['人物', ['person', 'person-gesture', 'person-role', 'person-fantasy',
+        'person-activity', 'person-sport', 'person-resting']],
+      ['衣服', ['clothing']],
+      ['动物', ['cat-face', 'monkey-face', 'animal-mammal', 'animal-bird',
+        'animal-amphibian', 'animal-reptile', 'animal-marine', 'animal-bug']],
+      ['植物', ['plant-flower', 'plant-other']],
+      ['自然', ['sky & weather', 'science']],
+      ['食物', [
+        'food-fruit', 'food-vegetable', 'food-prepared', 'food-asian',
+        'food-marine', 'food-sweet'
+      ]],
+      ['运动', ['sport', 'game']],
+    ]
+
+    return ()=> (
+      <div class={s.emojiList}>
+      <nav>
+        {table.map(item => <span>{item[0]}</span>)}
+      </nav>
+      <ol>
+        {table[refSelected.value][1].map(category => 
+          emojiList.find(item => item[0] === category)
+            ?.[1]
+            .map(item => <li>{item}</li>)
+        )}
+      </ol>
+    </div>
+    )
+  }
+})
